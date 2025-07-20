098d801501754001589cb172f26b85d2c6c75fef
Safiullah <110291911+safiullah28@users.noreply.github.com>
2025-07-20 14:45:26 +0500

    export app

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,11 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-
 import connectMongoDB from "./db/connectMongoDB.js";
 import msgRouter from "./routes/message.route.js";
-import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+connectMongoDB();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -22,15 +21,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const PORT = process.env.PORT;
+const app = express();
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // frontend Vite dev server
+    origin: "*",
     credentials: true,
   })
 );
@@ -40,7 +38,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
-app.use("/api/message", msgRouter);
+app.use("/api/messages", msgRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -50,7 +48,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectMongoDB();
-});
+export default app;
